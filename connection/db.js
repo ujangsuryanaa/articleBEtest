aeab64ebba5870bafce010015e9c119180dce13f
ujangsuyana <ujangsuryana588@gmail.com>
2022-11-01 15:40:08 +0700

    change database

--- connection/db.js
+++ connection/db.js
@@ -3,10 +3,10 @@ const {Pool} = require('pg')
 
 // setup connection pool
 const dbPool = new Pool({
-    database: 'sahaware',
+    database: 'dbl7tv3k7im4rc',
     port: 5432,
-    user: 'postgres',
-    password: '1234'
+    user: 'atwsrhykcnhojh',
+    password: '0c971cd5e397b3b3bbb32aaa8bfae38c37a0b89cdfb28b88d72e21b4328cb887'
 })
 
 // export db pool to be use for query
